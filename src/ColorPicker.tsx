d021224f377f1ce605da68e9a9da57f1c542413e
brian houston morrow <bhm@brianmorrow.net>
2022-11-11 00:50:32 +0000

    Fix: Show correct color by default in color picker.

--- src/ColorPicker.tsx
+++ src/ColorPicker.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent } from "react";
+import { ChangeEvent, useState } from "react";
 
 declare var window: any;
 
@@ -10,16 +10,23 @@ type ColorPickerProps = {
 
 export const ColorPicker = ({ label, defaultValue, onChangeColor }: ColorPickerProps): JSX.Element => {
     const convertedColor = window.w3color(defaultValue).toHexString();
+    const [colorValue, setColorValue] = useState(convertedColor);
 
     const handleOnChange = (event: ChangeEvent<HTMLInputElement>) => {
         const value = event.target.value;
         onChangeColor(value);
+        setColorValue(value);
     }
 
     return (
         <div>
-            <input type="color" id={label + "-input"} name={label} value={convertedColor} onChange={handleOnChange}></input>
-            <label htmlFor={label + "-input"}>{label}</label>
+            <input
+                type="color"
+                id={label + "-input"}
+                name={label}
+                value={colorValue}
+                onChange={handleOnChange}/>
+            <label htmlFor={label + "-input"}>{`${label} ${colorValue}`}</label>
         </div>
     );
 }
